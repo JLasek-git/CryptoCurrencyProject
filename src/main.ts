7d0352fffed503939d83ca55cd0a5b5314982d36
Jakub Lasek <jakublasek41@gmail.com>
2022-01-07 21:56:50 +0100

    CompositionApi fix

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import vueCompositionApi from '@vue/composition-api';
 
 Vue.config.productionTip = false;
+
+Vue.use(vueCompositionApi);
 
 new Vue({
   router,
--- src/plugins/compositionApi.ts
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import vueCompositionApi from '@vue/composition-api';
-
-Vue.use(vueCompositionApi);
