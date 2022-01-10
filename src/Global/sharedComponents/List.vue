e678ff9368a299388e120d4d4f4fb5a9212e4090
Jakub Lasek <jakublasek41@gmail.com>
2022-01-11 00:04:58 +0100

    Add vuescroll which changes scroll to custom

--- src/Global/sharedComponents/List.vue
+++ src/Global/sharedComponents/List.vue
@@ -1,22 +1,32 @@
 <template>
   <v-list class="main-list">
-    <v-list-item class="list-legend d-flex align-center"> </v-list-item>
+    <div class="list-legend d-flex justify-space-around align-center">
+      <div class="list-item-section">Name</div>
+      <div class="list-item-section">Price</div>
+      <div class="list-item-section">24h</div>
+      <div class="list-item-section">7d</div>
+      <div class="list-item-section">Cap</div>
+      <div class="list-item-section">Volume</div>
+      <div class="list-item-section">Circulation</div>
+    </div>
     <v-list-item-group class="list-items-container" v-model="selectedListItem">
-      <v-list-item
-        class="list-item__wrapper d-flex justify-space-around align-center"
-        active-class="item-active"
-        v-for="(item, i) in allListItems"
-        :key="i"
-        :value="item"
-      >
-        <div class="list-item-section">{{ item.name }}</div>
-        <div class="list-item-section">{{ item.price }}</div>
-        <div class="list-item-section">{{ item.day }}</div>
-        <div class="list-item-section">{{ item.week }}</div>
-        <div class="list-item-section">{{ item.cap }}</div>
-        <div class="list-item-section">{{ item.volume }}</div>
-        <div class="list-item-section">{{ item.circulation }}</div>
-      </v-list-item>
+      <vuescroll>
+        <v-list-item
+          class="list-item__wrapper d-flex justify-space-around align-center"
+          active-class="item-active"
+          v-for="(item, i) in allListItems"
+          :key="i"
+          :value="item"
+        >
+          <div class="list-item-section">{{ item.name }}</div>
+          <div class="list-item-section">{{ item.price }}</div>
+          <div class="list-item-section">{{ item.day }}</div>
+          <div class="list-item-section">{{ item.week }}</div>
+          <div class="list-item-section">{{ item.cap }}</div>
+          <div class="list-item-section">{{ item.volume }}</div>
+          <div class="list-item-section">{{ item.circulation }}</div>
+        </v-list-item>
+      </vuescroll>
     </v-list-item-group>
     <v-list-item class="list-navigation d-flex justify-end align-center">
       Navigation
@@ -25,7 +35,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from '@vue/composition-api';
+import { defineComponent, computed, ref } from '@vue/composition-api';
+import vuescroll from 'vuescroll';
 
 export default defineComponent({
   emits: ['input'],
@@ -50,6 +61,9 @@ export default defineComponent({
 
     return { allListItems, selectedListItem };
   },
+  components: {
+    vuescroll,
+  },
 });
 </script>
 
@@ -66,17 +80,28 @@ export default defineComponent({
     height: 30px;
     font-weight: bold;
     border-bottom: $thick-border;
+    padding: 0 $base-padding;
   }
 
+  & .list-item-section {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: calc(100% / 7);
+    color: var(--v-text-base);
+  }
   & .list-items-container {
     height: 85%;
     overflow: auto;
+
+    & .__bar-is-vertical {
+      background: var(--v-accent-base) !important;
+    }
+
     & .list-item__wrapper {
       border-bottom: $thick-border;
       height: 90px;
-      & .list-item-section {
-        color: var(--v-text-base);
-      }
+      padding: 0 $base-padding;
     }
   }
 }
