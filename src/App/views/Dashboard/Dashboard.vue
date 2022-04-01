da5f0f3f9d873873a3dbad45c2debcdf4a77d90c
Jakub <jakublasek41@gmail.com>
2022-04-01 21:09:05 +0200

    Created custom container component and styles

--- src/App/views/Dashboard/Dashboard.vue
+++ src/App/views/Dashboard/Dashboard.vue
@@ -1,15 +1,52 @@
 <template>
-  <div class="dashboard__wrapper">Dashboard</div>
+  <div class="dashboard__wrapper d-flex justify-space-between align-center">
+    <div class="multiple-items-row__container d-flex justify-space-between">
+      <CustomContainer />
+      <CustomContainer />
+      <CustomContainer />
+    </div>
+    <div class="long-row__container">
+      <CustomContainer />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api";
+import CustomContainer from "@/Global/sharedComponents/CustomContainer.vue";
 
 export default defineComponent({
   setup() {
     return {};
   },
+  components: {
+    CustomContainer,
+  },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dashboard__wrapper {
+  width: 100%;
+  height: 100%;
+  flex-wrap: wrap;
+
+  & .multiple-items-row__container {
+    width: 100%;
+    height: 50%;
+
+    & > * {
+      margin-left: 24px;
+
+      &:nth-child(1) {
+        margin-left: 0 !important;
+      }
+    }
+  }
+
+  & .long-row__container {
+    width: 100%;
+    height: 45%;
+  }
+}
+</style>
---
+++ src/Global/sharedComponents/CustomContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="custom-container__wrapper">Custom</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.custom-container__wrapper {
+  width: 100%;
+  height: 100%;
+  background-color: var(--v-secondary-base);
+  border: $base-border;
+  border-radius: $base-border-radius;
+}
+</style>
