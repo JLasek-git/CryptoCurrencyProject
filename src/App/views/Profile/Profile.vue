3372c70810566add2947b3f50482af6c8aa8327a
Jakub <jakublasek41@gmail.com>
2022-04-02 12:56:49 +0200

    Add tabsNavigation for Profile view

--- src/App/views/Profile/Profile.vue
+++ src/App/views/Profile/Profile.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="profile__wrapper">
+    <TabsNavigation
+      v-model="currentProfileWorkingMode"
+      :availableTabs="availableProfileModes"
+    />
     <CustomContainer :isIconVisible="false" />
   </div>
 </template>
 
 <script lang="ts">
 import CustomContainer from "@/Global/sharedComponents/CustomContainer.vue";
-import { defineComponent } from "@vue/composition-api";
+import TabsNavigation from "@/Global/sharedComponents/TabsNavigation.vue";
+import { defineComponent, ref } from "@vue/composition-api";
+import { availableProfileModes } from "@/App/views/Profile/data/profileModes";
+import { ProfileWorkingModeEnum } from "@/App/enums/ProfileEnums";
 
 export default defineComponent({
   setup() {
-    return {};
+    const currentProfileWorkingMode = ref(ProfileWorkingModeEnum.Profile);
+    return { availableProfileModes, currentProfileWorkingMode };
   },
-  components: { CustomContainer },
+  components: { CustomContainer, TabsNavigation },
 });
 </script>
 
 <style lang="scss" scoped>
 .profile__wrapper {
   height: 100%;
+  padding-bottom: 100px;
 }
 </style>
