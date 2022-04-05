5f665f234160b6efea91501c18c3a566db04f1d2
Jakub <jakublasek41@gmail.com>
2022-04-05 23:23:32 +0200

    Add meta

--- src/App/router/currencies.routes.ts
+++ src/App/router/currencies.routes.ts
@@ -8,9 +8,6 @@ export const currenciesRoutes: Array<RouteConfig> = [
     path: AppRoutesEnum.Currencies,
     name: AppRoutesNamesEnum.Currencies,
     component: () => import("@/App/views/Currencies/Currencies.vue"),
-<<<<<<< HEAD
-=======
     meta: { isNavigationDrawerVisible: true },
->>>>>>> fcce6f0abdab534171ce6a3a98990818aeab17e2
   },
 ];
