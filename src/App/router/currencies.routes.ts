d0f62f0ce6ef323e885498cbae90d43c5116cb39
Jakub <jakublasek41@gmail.com>
2022-04-05 23:16:40 +0200

    Small fixes for routes

--- src/App/router/currencies.routes.ts
+++ src/App/router/currencies.routes.ts
@@ -1,12 +1,12 @@
-import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import { AppRoutesEnum, AppRoutesNamesEnum } from '@/App/enums/AppRoutesEnums';
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import { AppRoutesEnum, AppRoutesNamesEnum } from "@/App/enums/AppRoutesEnums";
 
 Vue.use(VueRouter);
 export const currenciesRoutes: Array<RouteConfig> = [
   {
     path: AppRoutesEnum.Currencies,
     name: AppRoutesNamesEnum.Currencies,
-    component: () => import('@/App/views/Currencies/Currencies.vue'),
+    component: () => import("@/App/views/Currencies/Currencies.vue"),
   },
 ];
